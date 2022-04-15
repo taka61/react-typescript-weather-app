583cdf08439345910f9e0d50909c383c1f61f6e4
taka61 <taka.tennisloveup61@gmail.com>
2022-04-15 15:35:23 +0900

    データ取得中の表示を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import Loading from "./components/Loading";
 import './App.css';
-import { SemanticClassificationFormat } from "typescript";
 
 type ResultsStateType = {
   country: string;
@@ -14,6 +14,7 @@ type ResultsStateType = {
 }
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
   const [city, setCity] = useState<string>("");
   const [results, setResults] = useState<ResultsStateType>({
     country: "",
@@ -24,6 +25,7 @@ function App() {
   });
   const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     fetch(`https://api.weatherapi.com/v1/current.json?key=8b04c84be1d3487f90c40348221404&q=${city}&aqi=no`)
       .then(res => res.json())
       .then(data => {
@@ -35,6 +37,7 @@ function App() {
           icon: data.current.condition.icon
         })
         setCity("");
+        setLoading(false);
       })
       .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"))
   }
@@ -44,7 +47,7 @@ function App() {
       <div className="container">
         <Title />
         <Form setCity={setCity} getWeather={getWeather} city={city} />
-        <Results results={results} />
+        {loading ? <Loading /> : <Results results={results} />}
       </div>
     </div>
   );
